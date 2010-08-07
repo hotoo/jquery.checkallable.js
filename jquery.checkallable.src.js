9b2ae21800c1625b3aad11c363a12673991063a4
闲耘™ <hotoo.cn@gmail.com>
2010-08-07 13:47:22 +0800

    demo & shiftKey support.

--- jquery.checkallable.src.js
+++ jquery.checkallable.src.js
@@ -1,20 +1,45 @@
 /**
  * Check All checkbox.
+ * @author 闲耘 (hotoo.cn[AT]gmail.com)
+ * @version 1.0, 2010/07/07
  */
 (function($){
     $.fn.checkallable = function(option){
         return this.each(function(){
             var ME = this,
                 name = $(this).attr("name"),
-                checked = this.checked;
+                checked = this.checked,
+                lastCheck = null;
 
             defaults = $.extend(defaults, {"name":name}, option);
 
             $(this).click(function(){
                 checkAll(defaults.name, this.checked);
             });
-            $("input[type=checkbox][name="+defaults.name+"]").live("click", function(){
+            $("input[type=checkbox][name="+defaults.name+"]").live("click", function(evt){
+                // TODO: quick check by shift key.
                 if(this==ME){return;}
+                if(evt.shiftKey && lastCheck!=null && lastCheck!=this){
+                    var cs = $("input[type=checkbox][name="+name+"]"),
+                        state = lastCheck.checked;
+                    for(var i=0,inRange=false,l=cs.length; i<l; i++){
+                        if(cs[i]==lastCheck || cs[i]==this){
+                            if(!inRange){
+                                inRange = true;
+                                cs[i].checked = state;
+                                continue;
+                            }else{
+                                inRange = false;
+                                cs[i].checked = state;
+                                break;
+                            }
+                        }
+                        if(inRange){
+                            cs[i].checked = state;
+                        }
+                    }
+                }
+                lastCheck = this;
                 sync(ME, defaults.name);
                 if(defaults.callback){defaults.callback.call(this, this.checked);}
             });
